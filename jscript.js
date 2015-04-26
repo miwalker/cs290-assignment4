46048aa138c606861b5904fce22d821ffb44e5f5
Michael Walker <walkermi@onid.oregonstate.edu>
2015-04-26 17:50:14 -0500

    added buttons

--- jscript.js
+++ jscript.js
@@ -1,13 +1,38 @@
 var originalGistList = [];
 var gistList = document.getElementById("gist-list");
+var favList = document.getElementById("fav-list");
 
 //elDiv.innerHTML = test;
+
+
 
 function createList() {
 	for (var i = 0; i < originalGistList.length; i++) {
 		var tableData = document.createElement("tr");
 		var tableUrl = document.createElement("a");
 		var breaker = document.createElement("br");
+		var fbutton = document.createElement("button");
+		fbutton.innerHTML = "+";
+		fbutton.id = originalGistList[i].id;
+		/*
+		fubtton.onclick = function addToFavs() {
+			var favListID = this.id;
+			for (var ii = 0; ii < originalGistList.length; ii++) {
+				if (originalGistList[ii].id == favListID) {
+					var description = originalGistList[i].description;
+					tableUrl.href = originalGistList[i].url;
+					if (description === "") {
+						description = "No description provided";
+					}
+					var tableDataText = document.createTextNode(description);
+					tableUrl.appendChild(tableDataText);
+					gistList.appendChild(this);
+					gistList.appendChild(tableUrl);
+					gistList.appendChild(breaker);
+				}
+			}
+		};
+		*/
 		var description = originalGistList[i].description;
 		tableUrl.href = originalGistList[i].url;
 		if (description === "") {
@@ -15,6 +40,7 @@ function createList() {
 		}
 		var tableDataText = document.createTextNode(description);
 		tableUrl.appendChild(tableDataText);
+		gistList.appendChild(fbutton);
 		gistList.appendChild(tableUrl);
 		gistList.appendChild(breaker);
 	}
